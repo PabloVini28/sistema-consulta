cf4cdfb612623b73efa690c4ec79eb1acfdb8ca5
Pablo Vinas <pablo.araujo@alu.ufc.br>
2025-06-10 14:30:01 -0300

    feat: creating the register appointment body DTO

---
+++ src/main/java/com/scnuvem/sc/appointment/dtos/request/RegisterAppointmentDto.java
@@ -0,0 +1,28 @@
+package com.scnuvem.sc.appointment.dtos.request;
+
+import java.time.LocalDate;
+
+import com.scnuvem.sc.appointment.enums.DoctorSpecialty;
+import com.scnuvem.sc.appointment.enums.TimeEnum;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RegisterAppointmentDto(
+    @NotBlank(message = "Patient name cannot be blank")
+    String patientName,
+
+    @NotNull
+    DoctorSpecialty doctorSpecialty,
+
+    @NotNull(message = "Appointment time cannot be null")
+    TimeEnum appointmentTime,
+
+    @NotNull(message = "Appointment date cannot be null")
+    LocalDate appointmentDate,    
+
+    @NotBlank(message = "Description cannot be blank")
+    String description
+) {
+    
+}
